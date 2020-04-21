ef0a4e6cf044ae3e2d8bfb42a070aed3aa1a3a06
Perez Cato <perezcatoc@gmail.com>
2020-04-21 23:13:37 +0000

    added test to the project

--- index.js
+++ index.js
@@ -1,48 +1,4 @@
-import express from 'express';
-import morgan from 'morgan';
-import path from 'path';
-
-import { generateUrl,getUrl } from './helpers.js';
-
-const app = express();
-const __dirname = path.resolve();
-
-app.use(express.static(path.join(path.resolve(),'public')));
-app.use(morgan('tiny'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.get('/',(req,res) => {
-    res.sendFile('./public/index.html');
-});
-
-app.post('/shorten',async (req,res) => {
-    const { url } = req.body;
-    const shortUrl = await generateUrl(url);
-    console.log(shortUrl, '-' , url);
-
-    if( !shortUrl ) {
-        return res.status(422).json({
-            'error': 'Invalid Url'
-        });
-    }
-    return res.json({
-        url,
-        shortUrl,
-    })
-});
-
-app.get('/:link',async (req,res) => {
-    const urlLink = await getUrl(req.params.link);
-   if(urlLink){
-       return res.redirect(urlLink.site);
-   }
-    res.status(404).sendFile(__dirname+'/public/404.html');
-});
-
-app.get('*',(req,res) => {
-    return res.sendFile(__dirname+'/public/404.html');
-});
+import app from './server.js';
 
 app.listen(3000,() => {
     console.log('app is running at port 3000');
